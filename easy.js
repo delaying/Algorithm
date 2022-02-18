4b96bf01cdcc8d8a8ebd1b7bfaf5c6119172b127
delaying <pgy5638@naver.com>
2022-02-18 21:18:28 +0900

    입출력과 사칙연산

--- easy.js
+++ easy.js
@@ -40,15 +40,36 @@
 // console.log(`${input}??!`);
 
 
-//2741
-const fs = require('fs');
-const input = Number(fs.readFileSync('/dev/stdin').toString());
+// //2741
+// const fs = require('fs');
+// const input = Number(fs.readFileSync('/dev/stdin').toString());
 
-const n = input;
-let answer = '';
+// const n = input;
+// let answer = '';
 
-for(let i=1; i<=n; i++){
-    answer += i + "\n";
-}
+// for(let i=1; i<=n; i++){
+//     answer += i + "\n";
+// }
 
-console.log(answer);
+// console.log(answer);
+
+// //18108
+// const input = require('fs').readFileSync('/dev/stdin').toString();
+
+// console.log(input-543);
+
+
+
+// //10430
+// const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+
+// let A = Number(input[0]);
+// let B = Number(input[1]);
+// let C = Number(input[2]);
+
+// console.log((A+B)%C);
+// console.log(((A%C) + (B%C))%C);
+// console.log((A*B)%C);
+// console.log(((A%C) * (B%C))%C);
+
+
