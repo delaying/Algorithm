dcf7ca01c024aebf7d8ddcd76303aafeb4b72883
delaying <pgy5638@naver.com>
2022-03-06 17:00:41 +0900

    문자열 알고리즘 문제 연습

--- march.js
+++ march.js
@@ -177,3 +177,23 @@
 // else {
 // 	console.log(alpha[count.indexOf(max)].toUpperCase());
 // }
+
+// //2292
+// const fs = require("fs");
+// const input = (
+//   process.platform === "linux"
+//     ? fs.readFileSync("/dev/stdin").toString()
+//     : `1000000000`
+// ).trim();
+
+// let N = Number(input);
+
+// let i = 1;
+// let sum = 1;
+
+// while (sum < N) {
+//   sum += 6 * i;
+//   i++;
+// }
+
+// console.log(i);
