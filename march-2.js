5f4ee2b4840231030c21fbe9e5decaa6b402935d
delaying <pgy5638@naver.com>
2022-03-15 17:57:31 +0900

    재귀  알고리즘 문재 연습

--- march-2.js
+++ march-2.js
@@ -20,3 +20,29 @@
 // factorial(input);
 // console.log(sum);
 
+// //for 문 이용
+// const fs=require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+// let sum = 0;
+
+// function factorial(n){
+//     if(sum===0){
+//         sum = n;
+//     }else{
+//         sum *=n;
+//     }
+//     return sum;
+// }
+
+// for(let i=0;i<=input;i++){
+//     if(input==0){
+//         console.log(1);
+//         break;
+//     }
+//     factorial(i);
+// }
+
+// if(sum !==0){
+//     console.log(sum);
+// }
