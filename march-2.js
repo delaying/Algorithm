55f59941b821f8c73a0d9d81723f792508194852
delaying <pgy5638@naver.com>
2022-03-16 15:52:10 +0900

    재귀  알고리즘 문재 연습

--- march-2.js
+++ march-2.js
@@ -46,3 +46,46 @@
 // if(sum !==0){
 //     console.log(sum);
 // }
+
+
+// 10870
+//for문 이용
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+// let sum = [];
+// sum[0]=0;
+// sum[1]=1;
+
+// function fibonachi(n){
+//     for(let i=2; i<=n; i++){
+//         sum[i]= sum[i-1]+sum[i-2];
+//     }
+    
+//     console.log(sum[n]);
+//     return;
+    
+// }
+// fibonachi(Number(input));
+
+// //재귀이용
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+// let sum = [];
+// sum[input]=0;
+// sum[input-1]=1;
+
+// function fibonachi(n){
+//     if(n==0){
+//         console.log(sum[0]);
+//         return;
+//     }else{
+//         sum[n-2]= sum[n-1]+sum[n];
+//         fibonachi(n-1);
+//         return;
+//     }
+    
+// }
+
+// fibonachi(Number(input));
