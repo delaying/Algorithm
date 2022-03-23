99d3fad494abca44016398593e578cd435a308ee
delaying <pgy5638@naver.com>
2022-03-24 03:32:06 +0900

    정렬  알고리즘 문재 연습

--- march-2.js
+++ march-2.js
@@ -287,3 +287,17 @@
 //     if (String(num).includes("666")) count++;
 // }
 // console.log(num);
+
+
+//2750
+// const fs = require("fs");
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+
+// input.shift();
+
+// input.sort((a, b) => a - b);
+
+// for (let i = 0; i < input.length; i++) {
+//   console.log(input[i]);
+// }
