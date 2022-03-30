99571757b850a71e326b2c3580cb2f2cdb37875e
delaying <pgy5638@naver.com>
2022-03-30 21:07:13 +0900

    4월 수정

--- march-2.js
+++ march-2.js
@@ -323,20 +323,3 @@
 // const input = fs.readFileSync('/dev/stdin').toString().split('');
 
 // console.log(input.sort((a, b) => b - a).join(''));
-
-
-//11651
-// const fs = require('fs');
-// const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map(v=>v.trim()); 
-// const N = +input.shift();
-// const location = input.map(v=>v.split(' ').map(x=>+x));
-// const answer = location.sort((a,b)=>{
-//   if(a[1]>b[1]){
-//     return 1;
-//   }else if(a[1]<b[1]){
-//     return -1;
-//   }else{
-//     return a[0]-b[0];
-//   }
-// });
-// console.log(answer.map(v=>v.join(' ')).join('\n'));
