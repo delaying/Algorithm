9590be24d1b01529b241733b26ab94f0d9e1e833
delaying <pgy5638@naver.com>
2022-03-30 21:07:13 +0900

    정렬 알고리즘 풀이

--- march-2.js
+++ march-2.js
@@ -326,13 +326,17 @@
 
 
 //11651
-const fs=require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().split('\n');
-
-let arr=0;
-
-for(let i=0; i<input[0];i++){
-    if(input[i][1])
-}
-
-console.log(arr);
+// const fs = require('fs');
+// const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map(v=>v.trim()); 
+// const N = +input.shift();
+// const location = input.map(v=>v.split(' ').map(x=>+x));
+// const answer = location.sort((a,b)=>{
+//   if(a[1]>b[1]){
+//     return 1;
+//   }else if(a[1]<b[1]){
+//     return -1;
+//   }else{
+//     return a[0]-b[0];
+//   }
+// });
+// console.log(answer.map(v=>v.join(' ')).join('\n'));
