2e1bd8a9a9a665b765f613779d6dd2c8f64b2389
delaying <pgy5638@naver.com>
2022-04-03 22:28:08 +0900

    정렬 알고리즘

--- april.js
+++ april.js
@@ -13,3 +13,10 @@
 //   }
 // });
 // console.log(answer.map(v=>v.join(' ')).join('\n'));
+
+// //1181
+// const [n, ...words] = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+// Array.from(new Set(words))
+//     .sort((a, b) => a > b ? 1 : (a < b ? -1 : 0))
+//     .sort((a, b) => a.length - b.length)
+//     .forEach(i => console.log(i));
