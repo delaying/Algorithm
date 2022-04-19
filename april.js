13f9c492f5052a3b46a41cfba26f0f220046c3e2
delaying <pgy5638@naver.com>
2022-04-19 17:34:04 +0900

    5086 문제풀이

--- april.js
+++ april.js
@@ -61,6 +61,23 @@
 // }
 
 
+// //5086
+// const fs = require('fs');
+// const input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
+
+// input.pop();
+// const nums = input.map(v => v.split(' ').map(x => +x))
+
+// nums.forEach(v => {
+//   if (v[1] % v[0] == 0) {
+//     console.log('factor')
+//   } else if (v[0] % v[1] == 0) {
+//     console.log('multiple')
+//   } else {
+//     console.log('neither')
+//   }
+// })
+
 
 //2108
 const [n, ...num] = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n").map((e) => Number(e));
